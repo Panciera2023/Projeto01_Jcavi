dc3755676baf68368ed144676ac4638eae0188bb
Panciera-2022 <maicontgti@gmail.com>
2023-07-08 19:32:43 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Projeto supermercado
+#
+# Requisitos do Projeto utilizando conceitos de programação Procedural do Python.
+#
+# Requisitos:
+# Criar um Virtualenv para o Projeto
+# Criar os arquivos em estruturas de módulos importados no programa principal (main).
+# Efetuar ação de compra produtos e valores e armazenar no carrinho.(Armazenar em Estruturas de Dicionários).
+# Trabalhar estruturas de Exceções e Condicionais, Efetuando perguntas ao usuário e passando opções de execução na saída
+# e tratando as exceções. (EX: Terminou de Comprar ? Sim ou Não). (EX: Informe a Forma de Pagamento. E trate os erros de entrada invalida dos Usuários).
+# Após a conclusão de Compra imprimir em tela os itens comprados. E imprimir a soma total do valores comprados. (Pode utilizar Funções de MAP,REDUCE ou FILTER) para executar o calculo.
+# Na Hora do pagamento informar as opções de pagamento será Débito,Crédito ou Dinheiro. O Usuário deverá entrar com o valor da opção.
+# Caso a opção de Pagamento ser em dinheiro ou débito ganha desconto 5% do valor total do da Compra. Em crédito não terá desconto.Se informa opção invalida gerar mensagem de exceção ao usuário.
+#
+# Após concluir o pagamento armazenar os produtos comprados(Estrutura de Dicionário) em um arquivo csv. Armazenar as chaves e valores.
+# Não esqueça de criar um cabeçalho de identificação no arquivo CSV.
+
+
